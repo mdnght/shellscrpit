f05a89306dcc3f931112a2a38e813629a8feab44
mdnght <mdnght@sohu.com>
2020-02-05 17:38:10 +0800

    Update autocreateuser.sh

--- autocreateuser.sh
+++ autocreateuser.sh
@@ -2,19 +2,37 @@
 #Auto add Linux user
 #by authors Baif
 
-read -p "请输入要创建的用户名:"  username
+RANDOMPASSWD=`date +%s | sha256sum | base64 | head -c 16 ; echo` 
+
+read -p "请输入要创建的用户名:" username
 
 if [ -z $username ]; then
         echo " {例如}:$0 logs"
         exit 0
 else 
-        groupadd ${username}
-        useradd ${username} -g ${username} 
-                if [ $? -ne 0 ];then
-                        echo "添加的用户已经存在，请检查"
-                        exit 0
-                else
-                        passwd ${username}
-                fi
-        echo "用户${username}已经添加完毕"
-fi 
+        PS3=您确认要创建用户${username}:
+        select i in "Y" "N"
+        do
+                case $i in
+                        Y)
+                        groupadd ${username}
+                        useradd -r -g ${username} ${username} 
+
+                                if [ $? -ne 0 ];then
+                                        echo "添加的用户已经存在，请检查"
+                                        exit 0
+                                else
+                                        echo "建议用户${username}使用此随机密码:${RANDOMPASSWD}"
+                                        passwd ${username}
+                                fi
+                                echo "用户${username}已经添加完毕"
+                        ;;
+
+                        N)
+                        exit 
+                        ;;
+                esac
+        break
+        done
+
+fi  
